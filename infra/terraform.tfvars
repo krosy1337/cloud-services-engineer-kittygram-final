aba88bbbeadc183906297e32a78b27bc7847c8e4
Alexey Egorov <krosy1337@gmail.com>
2026-06-07 18:55:31 +0300

    сделал терраформ

---
+++ infra/terraform.tfvars
@@ -0,0 +1,4 @@
+cores = 4
+memory = 8
+disk_type = "network-hdd"
+disk_size = 50
---
+++ infra/variables.tf
@@ -0,0 +1,85 @@
+variable "vpc_name" {
+  description = "VPC Name"
+  type = string
+  default = "infra-network"
+}
+
+variable "net_cidr" {
+  description = "Subnet structure"
+  type = list(object({
+    name = string,
+    zone = string,
+    prefix = string
+  }))
+
+  default = [
+    { name = "infra-subnet-a", zone = "ru-central1-a", prefix = "10.129.1.0/24" },
+    { name = "infra-subnet-b", zone = "ru-central1-b", prefix = "10.130.1.0/24" },
+    { name = "infra-subnet-d", zone = "ru-central1-d", prefix = "10.131.1.0/24" },
+  ]
+}
+
+variable "vm_1_name" {
+  type = string
+  default = "vm-kittygram"
+}
+
+variable "aws_region" {
+  description = "AWS Region name"
+  type = string
+}
+
+variable "ssh_key" {
+  description = "SSH Public Key"
+  type = string
+}
+
+variable "cloud_id" {
+  description = "Cloud ID"
+  type = string
+}
+
+variable "folder_id" {
+  description = "Folder ID"
+  type = string
+}
+
+variable "image_family" {
+  type = string
+  default = "ubuntu-2404-lts"
+}
+
+variable "cores" {
+  type = number
+  default = 2
+}
+
+variable "memory" {
+  type = number
+  default = 2
+}
+
+variable "zone" {
+  type = string
+  default = "ru-central1-a"
+}
+
+variable "platform_id" {
+  type = string
+  default = "standard-v3"
+}
+
+variable "disk_type" {
+  type = string
+  default = "network-ssd"
+}
+
+variable "disk_size" {
+  type = number
+  default = 40
+}
+
+variable "nat" {
+  type = bool
+  default = true
+}
